f0eef8ee5af4cd5b813a1d25b71086fa2359bf9c
Nimesh <user.nimesh@gmail.com>
2025-10-29 16:23:39 +0530

    Prepare All tables

---
+++ app/Http/Controllers/RoleModuleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoleModule;
+use Illuminate\Http\Request;
+
+class RoleModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoleModule $roleModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoleModule $roleModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RoleModule $roleModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoleModule $roleModule)
+    {
+        //
+    }
+}
